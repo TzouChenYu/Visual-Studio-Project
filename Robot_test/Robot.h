8f638252e113e5623afdb63c2342d39982ac8ff2
TzouChenYu <ken82318@gmail.com>
2016-06-20 18:18:00 +0800

    build Robot library

---
+++ Robot_test/Robot.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <iostream>
+#include <vector>
+
+#include <Eigen\Dense>
+
+#define _CRT_SECURE_NO_WARNINGS
+
+
+#define pi 3.14159265359
+#define Deg2Rad(theta) (theta*pi/180) 
+#define Rad2Deg(theta) (theta*180/pi)
+
+namespace Rbt
+{
+
+
+}
